279d388620265fd30c62f8febc569740d383a727
Arthelon <hsing.daniel@gmail.com>
2016-08-01 16:28:11 +0800

    Wrote tests, minor fixes

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-module.exports = function(data) {
+module.exports = function(opts) {
+	if (!opts.paths) {
+		throw new Error("Please add at least one SPF path handler to middleware constructor")
+	}
+
     return function(req, res, next) {
         var spfQuery = req.query.spf
         var identifierMatch = false
         var identifiers = ["navigate", "prefetch", "navigate-back"]
 
-        if (data.identifiers && data.identifiers.length > 0) {
-            if (data.override) {
-                identifiers = data.identifiers
+        if (opts.identifiers && opts.identifiers.length > 0) {
+            if (opts.override) {
+                identifiers = opts.identifiers
             } else {
-                identifiers.concat(data.identifiers)
+                identifiers.concat(opts.identifiers)
             }
         }
         for (var i = 0; i < identifiers.length; i++) {
@@ -19,7 +23,7 @@ module.exports = function(data) {
 
         if (!identifierMatch) next()
         else {
-            var handler = data.paths[req.path]
+            var handler = opts.paths[req.path]
             if (!handler) {
                 next()
             } else if (typeof handler == "object") {
